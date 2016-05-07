3daf5db74e4380c7ec8798335a360d5ea02964b2
TheLucasMoore <lucasedwardmoore@gmail.com>
2016-05-07 15:28:26 -0500

    Done.

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -13,4 +13,8 @@
 //= require jquery
 //= require jquery_ujs
 //= require turbolinks
+//= require hello_world
+//= require learn
+//= require main
+//= require alert_maker
 //= require wrong_place_wrong_time

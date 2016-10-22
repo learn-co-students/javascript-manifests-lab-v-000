9cb7ccaebcd0b58713399b54f8102e4a8d5bb80b
DEREJE GEMECHU <dereje88@gmail.com>
2016-10-22 19:57:23 +0000

    Done.

--- app/assets/javascripts/admin.js
+++ app/assets/javascripts/admin.js
@@ -1,6 +1,3 @@
-// This is a manifest file that'll be compiled into application.js, which will include all the files
-// listed below.
-//
 // Any JavaScript/Coffee file within this directory, lib/assets/javascripts, vendor/assets/javascripts,
 // or any plugin's vendor/assets/javascripts directory can be referenced here using a relative path.
 //
@@ -13,6 +10,5 @@
 //= require jquery
 //= require jquery_ujs
 //= require turbolinks
-//= require alert_maker
-//= require wrong_place_wrong_time
-//= require main
+//= require hello_world
+//= require doesn_nothing

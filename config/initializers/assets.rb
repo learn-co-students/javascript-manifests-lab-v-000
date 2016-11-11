7822ff96b489c8a77a18513783cdcb341b5195a4
tpstar <hanlee12@gmail.com>
2016-11-10 21:45:32 -0500

    done

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -9,3 +9,10 @@ Rails.application.config.assets.precompile += %w( learn.js )
 # Precompile additional assets.
 # application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
 # Rails.application.config.assets.precompile += %w( search.js )
+Rails.application.config.assets.paths =
+[
+  "/Users/hanlee/Development/js/javascript-manifests-lab-v-000/app/assets/javascripts",
+  "/Users/hanlee/Development/js/javascript-manifests-lab-v-000/vendor/assets/javascripts"
+]
+
+# Rails.application.config.assets.paths << "New Path"

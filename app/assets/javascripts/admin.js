f20db2a3005188532858760c9ce0f424c98e99d5
mcclains2003 <mcclains2003@gmail.com>
2017-04-18 11:34:05 -0500

    Done.

--- app/assets/javascripts/admin.js
+++ app/assets/javascripts/admin.js
@@ -10,9 +10,5 @@
 // Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
 // about supported directives.
 //
-//= require jquery
-//= require jquery_ujs
-//= require turbolinks
-//= require wrong_place_wrong_time
-//= require alert_maker
-//= require main
+//= require hello_world
+//= require doesn_nothing

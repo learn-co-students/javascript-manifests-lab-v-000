2b3c75d1191f53877f5e9f364a19d4a6b7e4be3f
Jon <jon_p_stone@yahoo.com>
2017-10-19 22:53:51 -0500

    Done.

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -1,3 +1,4 @@
+
 // This is a manifest file that'll be compiled into application.js, which will include all the files
 // listed below.
 //
@@ -14,3 +15,5 @@
 //= require jquery_ujs
 //= require turbolinks
 //= require wrong_place_wrong_time
+//= require alert_maker
+//= require main

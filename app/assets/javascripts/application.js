1567723c5ebac35b3f2d1264c779ece5e1bebb34
johnsontaiwo <josseytee@gmail.com>
2017-11-16 18:32:39 -0500

    Done.

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -13,7 +13,6 @@
 //= require jquery
 //= require jquery_ujs
 //= require turbolinks
-//= require_tree .
-//= require wrong_place_wrong_time
 //= require main
 //= require alert_maker
+//= require wrong_place_wrong_time

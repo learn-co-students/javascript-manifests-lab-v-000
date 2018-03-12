3d61377fe01f19c479cc07418b98b86f3296da0d
Terry Blue <terryblue@comcast.net>
2018-03-12 22:24:00 +0000

    Done.

--- app/assets/javascripts/admin.js
+++ app/assets/javascripts/admin.js
@@ -10,3 +10,5 @@
 // Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
 // about supported directives.
 //
+//= require hello_world
+//= require doesn_nothing

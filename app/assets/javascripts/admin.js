0d64318530a0edf31754d45c349bef9a32b86004
nyuyz518 <lilyzhangyun@gmail.com>
2018-09-24 22:31:53 -0400

    Done.

--- app/assets/javascripts/admin.js
+++ app/assets/javascripts/admin.js
@@ -13,6 +13,7 @@
 //= require jquery
 //= require jquery_ujs
 //= require turbolinks
-//= require wrong_place_wrong_time
-//= require alert_maker
-//= require main  
+//= require hello_world
+//= require doesn_nothing
+//= require doesn_nothing
+//= require hello_world

68967110d86d98553b6542596a151a68a699dceb
FionaDL <github email address>
2019-05-07 14:15:13 +0200

    Done.

--- app/assets/javascripts/admin.js
+++ app/assets/javascripts/admin.js
@@ -13,7 +13,5 @@
 //= require jquery
 //= require jquery_ujs
 //= require turbolinks
-//= require wrong_place_wrong_time
 //= require hello_world
-//= require main
-//= require alert_maker
+//= require doesn_nothing
